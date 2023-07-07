c1bf442ea671cfc0efa385415f5262fc93a57cb3
MJSnowflakeAcademy <138872838+MJSnowflakeAcademy@users.noreply.github.com>
2023-07-07 13:32:35 +0200

    Update streamlit_app.py
    Updated menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title("My Parents New Healthly Dinner")
 
-streamlit.header("Breakfast menu")
-streamlit.text("Oatmeal")
-streamlit.text("Spinach")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
